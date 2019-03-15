dfc3be7594c40422c6d4b079c5507fb2c8473aff
georgi.stefanov <georgi.stefanov2001@gmail.com>
2019-03-15 18:48:06 +0200

    Added a 7 segment indicator

--- games/morse_code_game/morse_code_game.ino
+++ games/morse_code_game/morse_code_game.ino
@@ -34,7 +34,7 @@ char morse_code_mapping[30][5] = {
   "1100" //Z -> --..
 };
 
-
+//WE HAVE TO CHANGE THE PIN NUMBERS TO BE MORE SEQUENCIAL ONCE WE PUT EVERYTHING ON ONE ARDUINO !!!
 
 char input_word[3] = {'A','H','I'};
 int num_to_display = 5;
@@ -47,14 +47,55 @@ int dash_delay = 700;
 int pause_btw_letters = 1500;
 int pause_btw_reset = 3000;
 
+char Numbers[16][8] = {
+              "abcdef",
+              "bc",
+              "abged",
+              "abcgd",
+              "bcgf",
+              "afgcd",
+              "afedcg",
+              "abc",
+              "abcdefg",
+              "gfabcd",
+              "abcefg",
+              "fedcg",
+              "afed",
+              "bcdeg",
+              "afged",
+              "afge"         
+          };
+
+int OA1 = 0;
+int aPin = A2;
+int bPin = 2;
+int cPin = 4;
+int dPin = 5;
+int ePin = 6;
+int fPin = 8;
+int gPin = 9;
+
 void setup() {
   pinMode(potentiometer,INPUT);
-  pinMode(led,OUTPUT);
+  pinMode(aPin,OUTPUT);
+  pinMode(bPin,OUTPUT);
+  digitalWrite(aPin,HIGH);
+  digitalWrite(bPin,HIGH);
+  
+  for(int i = 4;i<10;i++){
+    pinMode(i,OUTPUT);
+    digitalWrite(i,HIGH);
+  }
+  
+  pinMode(OA1, OUTPUT);
+  digitalWrite(OA1, LOW);
+  digitalWrite(led,LOW);
+  
   Serial.begin(9600);
 }
 
 void loop() {
-  show_morse();
+  //show_morse();
   read_input();
 }
 
@@ -90,6 +131,50 @@ void morse_symbol(int delay_time){
 
 void read_input(){
   int t = analogRead(potentiometer);
-  //Serial.println(t);  
+  Serial.println(t);
+  t = map(t,0,1020,0,15);
+  Serial.println(t);
+  write_digit(Numbers[t]);
+  light_digit();  
   
+}
+
+void write_digit(char num[8]) {
+    for(int j = 0; j < strlen(num); j++){
+          char letter = num[j];
+        switch(letter){
+          case 'a':
+              digitalWrite(aPin, LOW);
+              break;
+          case 'b':
+              digitalWrite(bPin, LOW);
+              break;
+          case 'c':
+              digitalWrite(cPin, LOW);
+              break;
+          case 'd':
+              digitalWrite(dPin, LOW);
+              break;
+          case 'e':
+              digitalWrite(ePin, LOW);
+              break;
+          case 'f':
+              digitalWrite(fPin, LOW);
+              break;
+          case 'g':
+              digitalWrite(gPin, LOW);
+              break;
+        }
+    }   
+}
+
+void light_digit(){
+    digitalWrite(OA1, HIGH);
+    delay(3);
+    for(int i = 2; i < 10; i++){ 
+      if(i!=3 && i!=7){
+       digitalWrite(i, HIGH);
+      }
+   }
+   digitalWrite(A2,HIGH);
 }
