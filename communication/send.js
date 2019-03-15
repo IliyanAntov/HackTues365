bc452ad794156ba09ea26fa7f49e2429e005cb2a
Kristian-A <kristian.zh.apostolov@gmail.com>
2019-03-15 14:00:14 +0200

    Added the arduino side of the communication

--- communication/send.js
+++ communication/send.js
@@ -1,11 +1,11 @@
-let serialPort = require("serialport").SerialPort;
-
+const serialPort = require("serialport");
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const app = express();
 const port = 3000;
-let htmlPath = path.join(__dirname, 'index.html');
+
+const htmlPath = path.join(__dirname, 'index.html');
 fs.readFileSync(htmlPath);
 
 app.get('/', (req, res) => {
@@ -14,6 +14,17 @@ app.get('/', (req, res) => {
 
 app.get('/start', (req, res) => {
 	res.send('Game starting..');
+});
+
+let arduinoPort = new serialPort('/dev/ttyACM1', {
+    baudRate: 9600
+});
+
+arduinoPort.write('hello from node\n', err => {
+    if (err) {
+      return console.log('Error on write: ', err.message);
+    }
+    console.log('message written');
 });
 
 app.listen(port, () => console.log(`Server started`));
