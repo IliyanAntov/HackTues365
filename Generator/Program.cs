1ea27d512a8e5accf4fcee47081da2debaa0d55b
Antoan Yanev <antoan.yanev5@gmail.com>
2019-03-15 19:58:44 +0200

    Add files via upload

---
+++ Generator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO.Ports;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            SerialPort port;
+
+            port = new SerialPort("COM7", 9600);
+            port.Open();
+
+            port.Write("1");
+        }
+    }
+}
