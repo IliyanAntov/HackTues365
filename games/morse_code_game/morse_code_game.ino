1f8f044fcc69ef22c022affa422d8d1b21eace45
georgi.stefanov <georgi.stefanov2001@gmail.com>
2019-03-15 16:57:09 +0200

    Made it possible to have all characters in the alphabet usable in morse

--- games/morse_code_game/morse_code_game.ino
+++ games/morse_code_game/morse_code_game.ino
@@ -1,18 +1,42 @@
 //dot - 0
 //dash - 1
+
 //hardcode incoming
 // | | |
 // v v v
 
-char morse_code_mapping[3][10] = {
+char morse_code_mapping[9][10] = {
   "01", //A -> .-
   "1000", //B -> -...
-  "1010" //C -> -.-.
+  "1010", //C -> -.-.
+  "100", //D -> -..
+  "0", //E -> .
+  "0010", //F -> ..-.
+  "110", //G -> --.
+  "0000", //H -> ....
+  "00", //I -> ..
+  "0111", //J -> .---
+  "101", //K -> -.-
+  "0100", //L -> .-..
+  "11", //M -> --
+  "10", //N -> -.
+  "111", //O -> --- 
+  "0110" //P -> .--.
+  "1101", //Q -> --.-
+  "010", //R -> .-.
+  "000", //S -> ...
+  "1", //T -> -
+  "001", //U -> ..- 
+  "0001", //V -> ...-
+  "011", //W -> .--
+  "1001", //X -> -..-
+  "1011", //Y -> -.--
+  "1100" //Z -> --..
 };
 
 
 
-char input_word[3] = {'A','B','C'};
+char input_word[3] = {'A','H','I'};
 int num_to_display = 5;
 
 int potentiometer = A0;
@@ -34,7 +58,7 @@ void loop() {
 }
 
 void show_morse(){
-  for(int i = 0;i<3;i++){
+  for(int i = 0;i<9;i++){
     //words
     size_t symbol_size = sizeof(morse_code_mapping[i])/sizeof(char); //CHECK IF THE SIZE CALCULATION IS RIGHT
     Serial.println(symbol_size);
