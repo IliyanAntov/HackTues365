ab7fce5f6eb9e365129a731ab374a86775f7deca
georgi.stefanov <georgi.stefanov2001@gmail.com>
2019-03-16 13:32:16 +0200

    Implementing the most beautiful code ever

--- games/allGames/allGames.ino
+++ games/allGames/allGames.ino
@@ -1,17 +1,110 @@
 #include "Delay.h"
-#include "Game.h"
 
-Delay h;
-Game *game;
+//BUTTON GAME PARAMS
+
+int timeToPress = 3;
+char colors[4] = {'R','G','B','Y'};
+int buttonPin = 3;
+int common_cathode = 10;
+int red_anode = 11;
+int green_anode = 12;
+int blue_anode = 13;
+
+int fakeTimer = 3;
+
+int buttonState = 1;
+
+
+
+
+//MORSE GAME PARAMS
+
+
 
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(9600);
+
+    initButtonGame();       
 
 }
 
 
 
 void loop() {
-  // put your main code here, to run repeatedly:
+//   if(buttonGame == 1){
+//       Serial.println("brao ludko");
+//   }else if(buttonGame == -1){
+//       Serial.println("Kur");
+//   }
+    buttonGame();
+}
+
+
+
+void buttonGame() {
+    if (!digitalRead(buttonPin) && buttonState == 1) {
+        buttonState = -1;
+        if (fakeTimer == timeToPress) {
+            //right moment
+            int index = random(4);
+            char color = colors[index];
+            while (buttonState == -1) {
+                lightRGB(color);
+                if (digitalRead(buttonPin)) {
+                    buttonState = 1;
+                    turnOffRGB();
+                    if (fakeTimer == timeToPress) {
+                        // return 1;
+                    } else {
+                        // return -1;
+                    }
+                }
+            }
+        } else {
+            turnOffRGB();
+            // return -1;
+        }
+    }
+    // return 0;
+}
+
+void lightRGB(char color) {
+  switch(color){
+    case 'R':
+      turnOnRGB(1,0,0);
+      break;
+    case 'G':
+      turnOnRGB(0,1,0);
+      break;
+    case 'B':
+      turnOnRGB(0,0,1);
+      break;
+    case 'Y':
+      turnOnRGB(1,1,0);
+      break;
+  }
+}
+
+void turnOnRGB(int R, int G, int B){
+  digitalWrite(red_anode,R);
+  digitalWrite(green_anode,G);
+  digitalWrite(blue_anode,B);
+  digitalWrite(common_cathode,LOW);
+}
+
+void turnOffRGB(){
+  digitalWrite(red_anode,LOW);
+  digitalWrite(green_anode,LOW);
+  digitalWrite(blue_anode,LOW);
+  digitalWrite(common_cathode,HIGH);
+}
+
+void initButtonGame(){
+    randomSeed(analogRead(0));
+    pinMode(buttonPin,INPUT);
+    for (int i = 10;i<14;i++) {
+        pinMode(i,OUTPUT);
+    }
+    turnOffRGB();
 
 }
