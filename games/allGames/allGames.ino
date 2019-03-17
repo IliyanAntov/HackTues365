be94adc2fda53dc9367cc784b3fdad638411c1ce
georgi.stefanov <georgi.stefanov2001@gmail.com>
2019-03-17 03:45:56 +0200

    Win/Lose condition

--- games/allGames/allGames.ino
+++ games/allGames/allGames.ino
@@ -4,28 +4,89 @@
 #include "Timer.h"
 #include "ArduinoRead.h"
 
-void setup() {
-   setupTimer();
-   setupReading();
-   while (!loadInputs()) {Serial.print("loading");}
+int tries = 2;
+int redLeds[tries] = {41, 42};
+int greenLeds[3] = {38,39,40};
+bool gameOver = false;
 
-   setupButtonGame(RGBColor, wantedDigit);
+int tickMorseCodeReturn;
+int tickButtonGameReturn;
+int tickSimonSaysGameReturn;
+
+bool completed[3] = {false, false, false};
+
+void setup() {
+    for (int i = 38; i < 43; i++) {
+        pinMode(i, OUTPUT);
+        digitalWrite(i, LOW);
+    }
+
+    setupTimer();
+    setupReading();
+    while (!loadInputs()) {Serial.print("loading");}
+
+    setupButtonGame(RGBColor, wantedDigit);
 //   setupMorseCode(wordIndex, morseCodeWord);
 //   setupSimonSaysGame(buttonOrder, sequence);
 }
 
 void loop() {
-    if (tickTimer()) {
-        if (tickMorseCode() < 0) {
-            //wrong
-        }
-        if (tickButtonGame() < 0) {
+    displayStrikes(tries);
+    if (tickTimer() && !gameOver) { //wrong
+        tickMorseCodeReturn = tickMorseCode();
+        tickButtonGameReturn = tickButtonGame();
+        tickSimonSaysGameReturn = tickSimonSaysGame();
 
+        if (!completed[0]) {
+            if (tickMorseCodeReturn < 0) {
+                checkTries();
+            } else if (tickMorseCodeReturn == 1) {  //Game completed
+                completed[0] = true;
+            }
+        } else {
+            digitalWrite(greenLeds[0], HIGH);
         }
-        if (tickSimonSaysGame() < 0) {
-            //wrong
+        if (!completed[1]) {
+            if (tickButtonGameReturn < 0) {
+                checkTries();
+            } else if (tickButtonGameReturn == 1) {
+                completed[1] = true;
+            }
+        } else {
+            digitalWrite(greenLeds[0], HIGH);
+        }
+        if (!completed[2]) {
+            if (tickSimonSaysGameReturn < 0) {
+                checkTries();
+            } else if (tickSimonSaysGameReturn == 1) {
+                completed[2] = true;
+            }
+        } else {
+            digitalWrite(greenLeds[0], HIGH);
         }
     } else {
-        
+        //GAME OVER
+        Serial.println("Game over");
+    }
+}
+
+void checkTries() {
+    if (tries >= 1) {
+        tries--;
+    } else {
+        gameOver = true;
+    }
+}
+
+void displayStrikes(int strikes) {
+    switch (strikes) {
+        case 1:
+            digitalWrite(redLeds[0], HIGH);
+            digitalWrite(redLeds[1], LOW);
+            break;
+        case 2:
+            digitalWrite(redLeds[0], HIGH);
+            digitalWrite(redLeds[1], HIGH);
+            break;
     }
 }
